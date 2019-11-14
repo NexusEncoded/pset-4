251631a0f5537d4fc563f0a530640fa887339153
Ellis Halliday <1478pop@gmail.com>
2019-11-14 13:22:52 -0500

    One Done

--- src/sum.js
+++ src/sum.js
@@ -1,10 +1,25 @@
 const readlineSync = require("readline-sync");
 
-let lower1 = Number(readlineSync.question("Lower bound: "));
-let upper1 = Number(readlineSync.question("Upper bound: "));
-let lower2 = Number(readlineSync.question("Lower bound: "));
-let upper2 = Number(readlineSync.question("Upper bound: "));
+let sum = 0;
+let x = 0;
 
+console.log("")
+do{
+  lower1 = Number(readlineSync.question("Lower bound: "));
+  upper1 = Number(readlineSync.question("Upper bound: "));
+} while (lower1 < Number.MIN_SAFE_INTEGER || upper1 > Number.MAX_SAFE_INTEGER || lower1 >= upper1 || Number.isNaN(lower1) || Number.isNaN(upper1) || lower1 % 1 != 0 || upper1 % 1 != 0)
 
-if (numba < Number.MIN_SAFE_INTEGER){
-  console.log("\nInvalid.")
+for (let x = lower1; x <= upper1; x++) {
+if (lower1 % 2 === 0){
+  for (x = lower1; x <= upper1; x+=2){
+    sum = x + sum;
+  }
+}
+}
+if (lower1 % 2 !== 0){
+  for (x = lower1 + 1; x <= upper1; x+=2){
+    sum = x + sum;
+  }
+}
+sum = sum.toLocaleString("en");
+console.log("\n" + sum + ".");
