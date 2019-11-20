c4a600ba1135583579866d8c3cabeecd5abd00ba
Ellis Halliday <1478pop@gmail.com>
2019-11-20 13:15:18 -0500

    Done with Average

--- src/average.js
+++ src/average.js
@@ -1,5 +1,18 @@
 const readlineSync = require("readline-sync");
+let average = 0
+let sum = 0
+divide = 0
 
+console.log();
 do{
-  numba = Number(readlineSync.question("Positive integer: "));
-} while (numba < 1 || numba > Number.MAX_SAFE_INTEGER || numba < Number.MIN_SAFE_INTEGER || Number.isNaN(numba) || numba % 1 != 0)
+  numba = Number(readlineSync.question("Non-negative integer: "));
+  if (numba >= 0 && numba <Number.MAX_SAFE_INTEGER && !Number.isNaN(numba) && numba % 1 == 0) {
+    sum = numba + sum
+    divide +=1
+  }
+} while ((numba >= 0 && numba < Number.MAX_SAFE_INTEGER &&  numba % 1 == 0) || Number.isNaN(numba) || divide == 0)
+
+average = sum/divide
+
+average = average.toLocaleString('en', {minimumFractionDigits: 3, maximumFractionDigits: 3});
+console.log("\n" + average +  ".");
